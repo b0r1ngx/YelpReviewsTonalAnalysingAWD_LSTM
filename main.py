60b4ff6e5b6b0dcbd4c47af8a59ae095bf52c62e
boringx <perkeboring@gmail.com>
2023-08-14 16:45:58 +0300

    Add link to paper

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 # Model input: reviews on service work
 # Sentiment analysing
 # Model type: AWD-LSTM
+
+# Papers:
+# Regularizing and Optimizing LSTM Language Models
+# https://arxiv.org/pdf/1708.02182v1.pdf
