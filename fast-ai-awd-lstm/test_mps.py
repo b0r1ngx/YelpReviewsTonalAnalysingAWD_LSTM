b7bdae9b20a019a7606c5c29c76a10225c747fa7
boringx <perkeboring@gmail.com>
2023-10-03 23:21:56 +0300

    obsolete

--- fast-ai-awd-lstm/test_mps.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    _td = torch.device(torch.cuda.current_device())
-    test_eq(default_device(-1), _td)
-    test_eq(default_device(True), _td)
-else:
-    test_eq(default_device(False), torch.device('cpu'))
-default_device(-1);
